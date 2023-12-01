55b49f2e8c42f2fa7f75d4b86ed0fd424bb229fa
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-12-01 23:37:05 +0100

    Update refined to 0.9.29 (#20)

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val skunk     = "0.2.3"
   val shapeless = "2.3.7"
   val newtype   = "0.4.4"
-  val refined   = "0.9.28"
+  val refined   = "0.9.29"
 
   val munit = "0.7.29"
 }
