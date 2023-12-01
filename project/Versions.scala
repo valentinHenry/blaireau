aee52b5b5f7d4c276c15b9e961ccd53767032f15
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-12-01 23:45:26 +0100

    Update skunk-circe, skunk-core to 0.6.2 (#46)
    Co-authored-by: Firiath <32574451+valentinHenry@users.noreply.github.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,7 @@
 object Versions {
-  val skunk     = "0.2.3"
+  val skunk     = "0.6.2"
   val shapeless = "2.3.10"
   val newtype   = "0.4.4"
   val refined   = "0.9.29"
-
-  val munit = "0.7.29"
+  val munit     = "0.7.29"
 }
